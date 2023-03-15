abda43f4f2418e468880f829c397d9a3c6913afa
antonhibl <antonhibl11@gmail.com>
2023-03-15 14:15:38 -0700

    updated tests functions with a default regexp and etc.
        regexp can be used to specify which mission's/application's tests you want
        to run.

--- lisp/test.el
+++ lisp/test.el
@@ -2,10 +2,8 @@
   "Run ISIS3 ctests, optionally pass a regex to specify which tests to run.
 Argument REGEX regex pattern to specify which tests to run, defaults to all."
   (interactive
-   (list ((read-string "ctests regex: " "\\."))))
+   (list (read-string "ctests regex: " "\\.")))
   (let ((ctest-cmd "ctest -VV -R"))
     ;; change the directory to $ISISROOT
     (async-shell-command (format "cd $ISISROOT;%s \"%s\"" ctest-cmd regex)))
   )
-
-(astrolisp-isis-ctests "\\TgoCassisstitch.")
